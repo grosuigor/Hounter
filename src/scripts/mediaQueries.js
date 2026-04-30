bf09d026ecc88c4d012f500ca09adc729676c15c
Grosu Igor <igrosu@griddynamics.com>
2026-04-30 18:12:00 +0300

    fix(header): fixed stats carousel

--- src/scripts/mediaQueries.js
+++ src/scripts/mediaQueries.js
@@ -2,5 +2,6 @@ export function getViewportSize() {
   if (window.matchMedia("(max-width: 65em)").matches) return "xs";
   if (window.matchMedia("(max-width: 80em)").matches) return "sm";
   if (window.matchMedia("(max-width: 90em)").matches) return "md";
-  return "lg";
+  if (window.matchMedia("(max-width: 100em)").matches) return "lg";
+  return "xl";
 }
