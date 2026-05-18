ddcc08d9b4b89724cb794e28e5ad02ca9d50b6d2
Grosu Igor <igrosu@griddynamics.com>
2026-05-18 14:41:50 +0300

    feat(testimonials): enabled snapping into position after scrolling

--- src/scripts/components/testimonialsCarousel/handler.js
+++ src/scripts/components/testimonialsCarousel/handler.js
@@ -1,21 +1,37 @@
 const section = document.querySelector(".testimonials");
-const carousel = section.querySelector(".carousel__track");
+const carousel = section.querySelector(".carousel");
+const carouselTrack = section.querySelector(".carousel__track");
 const dots = Array.from(section.querySelectorAll(".carousel__dot"));
 
-const reviewCount = carousel.childElementCount;
+const reviewCount = carouselTrack.childElementCount;
 let currentIndex = 1;
 
-function updateCarouselPosition() {
-  const parent = carousel.parentElement;
-  const scrollProgress =
-    currentIndex === 0
-      ? 0
-      : (parent.scrollWidth - parent.clientWidth) / (currentIndex === 1 ? 2 : 1);
+function calcCarouselScrollableWidth() {
+  return carousel.scrollWidth - carousel.clientWidth;
+}
 
-  parent.scrollTo({
-    left: scrollProgress,
+let carouselScrollableWidth = calcCarouselScrollableWidth();
+let isUserScrolling = true;
+let lastScrollLeft = 0;
+
+function updateCarouselPosition() {
+  let scrollProgress = 1 / (reviewCount - currentIndex);
+
+  if (currentIndex === 0) {
+    scrollProgress = 0;
+  }
+
+  lastScrollLeft = carouselScrollableWidth * scrollProgress;
+
+  isUserScrolling = false;
+  carousel.scrollTo({
+    left: lastScrollLeft,
     behavior: "smooth",
   });
+
+  setTimeout(() => {
+    isUserScrolling = true;
+  }, 1000);
 }
 
 function gotoReview(index) {
@@ -33,8 +49,29 @@ function gotoReview(index) {
   updateCarouselPosition();
 }
 
+function updateCurrentDot(e) {
+  if (!isUserScrolling) {
+    return;
+  }
+
+  let index = Math.min(
+    Math.floor((e.target.scrollLeft / carouselScrollableWidth) * reviewCount),
+    reviewCount - 1,
+  );
+
+  if (index === currentIndex) {
+    const scrollDirection = Math.sign(e.target.scrollLeft - lastScrollLeft);
+    index += scrollDirection * 1;
+  }
+
+  gotoReview(index);
+}
+
 dots.forEach((dot, i) => dot.addEventListener("click", () => gotoReview(i)));
+carousel.addEventListener("scrollend", updateCurrentDot);
+
+updateCarouselPosition();
 
 new ResizeObserver(() => {
-  requestAnimationFrame(updateCarouselPosition);
+  requestAnimationFrame(calcCarouselScrollableWidth);
 }).observe(carousel);
